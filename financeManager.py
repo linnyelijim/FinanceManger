29ce5e628b5e4cedcec1ab852dbef7874bc42cf3
Lindsey Jimenez <lindseyjimenez@gmail.com>
2024-01-03 20:32:36 -0500

    Converted date to month and added sum of amounts.

--- financeManager.py
+++ financeManager.py
@@ -7,19 +7,34 @@ lindsey = ["LJ_capOne_2022.csv", "LJ_capOne_2023.csv"]
 months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 transactions = []
 
-
+# Changes the date to a month
+def switch(date):
+    try:
+        parsed_date = datetime.strptime(date, '%Y-%m-%d')
+        month_number = parsed_date.month
+        month = months[month_number - 1]
+        return month
+    
+    except ValueError:
+        print("Invalid date format. Please use 'YYYY-MM-DD'.")
+        return "undefined"
+    
 # Opens a .csv file and extracts each transaction
 def financeTracker(file):
+    sum = 0.0
     with open(file, mode='r') as csv_file:
         csv_reader = csv.reader(csv_file)
         for row in csv_reader:
             date = row[0]
+            month = switch(date)
             name = row[3]
             amount = float(row[5] if row[5] != '' else '-'+ row[6])
+            sum += amount if amount > 0 else 0
             category = row[4] 
-            transaction = ((date, name, category, amount))
+            transaction = ((month, name, category, amount))
             print(transaction)
             transactions.append(transaction)
+        print(sum)
         return transactions
 
 # Connects to a Google Spreadsheet         
